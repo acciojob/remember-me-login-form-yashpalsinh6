da58f279a695e7a54a729488b18f9348323fc130
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-03 13:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-//your JS code here. If required.
-document.getElementById('loginform').addEventListener('submit',function(event)
-{
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let submitBtn = document.getElementById('submit');
+let checkBox = document.getElementById('checkbox');
+
+submitBtn.addEventListener('click',(event)=>{
 	event.preventDefault();
-	var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var remember = document.getElementById('checkbox').checked;
-
-	if (remember) {
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-} else {
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
+	if (username.value === "" || password.value === "") {
+		return;
+	}
+	let user = {
+		name:username.value,
+		pass:password.value,
+	}
+	if (checkBox.checked) {
+		localStorage.setItem('test',JSON.stringify(user));
+	}
+	alert(`Logged in as ${username.value}`);
+	username.value = "";
+	password.value = "";
+});
+		
+var data = localStorage.getItem('test');
+if (data !== null) {
+	let btn = document.createElement('button');
+	btn.setAttribute('id','existing');
+	btn.innerText = 'Login as existing user';
+	document.querySelector('form').append(btn);
+	btn.addEventListener('click',()=>{
+		let existname = JSON.parse(data);
+		alert(`Logged in as ${existname.name}`);
+	});
 }
-
-alert('Logged in as ' + username);
-});
-var btn;
-window.onload = function() {
-    if (localStorage.getItem('username')) {
-        btn = document.createElement('button');
-        btn.id = 'existing';
-        btn.textContent = 'Login as existing user';
-        document.body.appendChild(btn);
-    }
-	btn.addEventListener('click', function() {
-    alert('Logged in as ' + localStorage.getItem('username'));
-});
-};
-
-
